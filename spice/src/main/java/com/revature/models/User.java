1b2be87c8aca9e2792cf9abce606e1375b3da025
Hannah Brown <2hbrown13@gmail.com>
2020-12-17 20:24:21 -0600

    getting Spicy

---
+++ spice/src/main/java/com/revature/models/User.java
@@ -0,0 +1,107 @@
+package com.revature.models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class User {
+	
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue
+	private int user_id;
+	@Column(name="first_name", nullable=false)
+	private String firstName;
+	@Column(name="last_name", nullable=false)
+	private String lastName;
+	@Column(name="bio")
+	private String bio;
+	@Column(name="profile_pic")
+	private String profilePicture;
+	@Column(name="username", nullable=false)
+	private String username;
+	@Column(name="password", nullable=false)
+	private String password;
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int user_id, String firstName, String lastName, String bio, String profilePicture, String username,
+			String password) {
+		super();
+		this.user_id = user_id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.bio = bio;
+		this.profilePicture = profilePicture;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getUserId() {
+		return user_id;
+	}
+
+	public void setUserId(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", firstName=" + firstName + ", lastName=" + lastName + ", bio=" + bio
+				+ ", profilePicture=" + profilePicture + ", username=" + username + ", password=" + password + "]";
+	}
+	
+	
+	
+}
