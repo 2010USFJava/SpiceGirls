cab57d2ce76ef5c3e61e3edc5ec71bf204776632
mounkeygirl <mounkeygirl@yahoo.com>
2020-12-19 13:01:13 -0600

    Added my SQL Editor to resources

---
+++ spice/src/main/resources/AnnasSql.sql
@@ -0,0 +1,5 @@
+select * from users;
+select * from login;
+
+insert into users values(2,'Anna','Carlson','The one doing the updates',null);
+insert into login values(2,'scary_spice','spookyScarySkeletons');
