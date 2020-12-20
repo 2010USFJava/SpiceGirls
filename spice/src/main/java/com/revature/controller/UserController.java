64478e8b6ea603a2770dcdb41347b29236c50c0f
Isa <isakaybeckman@gmail.com>
2020-12-19 20:10:11 -0600

    angular interfaces

--- spice/src/main/java/com/revature/controller/UserController.java
+++ spice/src/main/java/com/revature/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.models.User;
 import com.revature.services.UserService;
 
-//@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController //could also use controller but we are using rest as well
 @RequestMapping("/users") //takes the place of url pattern inside servlet 
 public class UserController {
