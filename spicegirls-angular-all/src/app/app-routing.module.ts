357f0d95490d29737ba5432c420499852dc95778
mounkeygirl <mounkeygirl@yahoo.com>
2020-12-26 13:26:18 -0600

    reinserted update controller

--- spicegirls-angular-all/src/app/app-routing.module.ts
+++ spicegirls-angular-all/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FeedComponent } from './feed/feed.component';
+// import { FeedComponent } from './feed/feed.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
